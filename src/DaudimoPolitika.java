2cff6f03aacd988283fb88f7faf09741894b23cb
Poleninas <adomas202@gmail.com>
2018-10-03 23:25:52 +0300

    hmmm...

---
+++ src/DaudimoPolitika.java
@@ -0,0 +1,4 @@
+public interface DaudimoPolitika {
+    public float RizikaRaudona(float galingumas);
+    public float RizikaZalia(float galingumas);
+} // Nezinau, kur sita dar implementuoti, bet tikriausiai lygiai taip pat, kaip ir ta kito interface
